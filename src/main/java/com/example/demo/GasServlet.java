aa02c70aac3fdc32026fa6dfc7cf5f51ffcaf685
Eric Lau <ericglau@outlook.com>
2020-03-24 22:45:02 -0400

    Update servlet name, refactor

--- src/main/java/com/example/demo/GasServlet.java
+++ src/main/java/com/example/demo/GasServlet.java
@@ -12,12 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 import com.example.demo.api.ApiException;
 import com.example.demo.api.Web3Api;
 import com.example.demo.models.GasPrediction;
-import com.example.demo.models.Payload;
+import com.example.demo.models.GasPredictions;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-@WebServlet(urlPatterns="/servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet(urlPatterns="/gas")
+public class GasServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     private static final long GWEI = 1000000000;
@@ -28,16 +28,16 @@ public class HelloServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        Payload payload;
+        GasPredictions gasPredictions;
         try {
-            payload = api.gasPredictions("UAK000000000000000000000000demo0001", "ethereum-mainnet");
+            gasPredictions = api.gasPredictions("UAK000000000000000000000000demo0001", "ethereum-mainnet").getPayload();
         } catch (ApiException e) {
             response.getWriter().append("Web3API returned with exception: " + e.getMessage());
             return;
         }
-        GasPrediction fast = payload.getPayload().getFast();
-        GasPrediction average = payload.getPayload().getAverage();
-        GasPrediction safeLow = payload.getPayload().getSafeLow();
+        GasPrediction fast = gasPredictions.getFast();
+        GasPrediction average = gasPredictions.getAverage();
+        GasPrediction safeLow = gasPredictions.getSafeLow();
 
         response.getWriter().append("Fast: " + (fast.getGasPrice().longValue() / GWEI) + " Gwei, " + fast.getWait() + " minutes wait\n");
         response.getWriter().append("Average: " + (average.getGasPrice().longValue() / GWEI) + " Gwei, " + average.getWait() + " minutes wait\n");
